9f13b5bc0056058a17dfa0f1022ce774381a34ff
MisterT0815 <larsdebbrecht@gmail.com>
2023-04-06 12:40:17 +0200

    fixed TestCharacterliteralMachine class, no inner class

--- TestCharacterliteralMachine.java
+++ TestCharacterliteralMachine.java
@@ -2,15 +2,10 @@ import java.io.OutputStream;
 
 public class TestCharacterliteralMachine {
 
-    public class TestABMachine {
-
-        public void test( String input ) throws Exception {
-            compiler.StateMachine characterliteralMachine = new compiler.machines.CharacterliteralMachine();
-            java.io.OutputStreamWriter outWriter = new java.io.OutputStreamWriter(System.out);
-            characterliteralMachine.process( input, outWriter);
-        }
-
+    public void test( String input ) throws Exception {
+        compiler.StateMachine characterliteralMachine = new compiler.machines.CharacterliteralMachine();
+        java.io.OutputStreamWriter outWriter = new java.io.OutputStreamWriter(System.out);
+        characterliteralMachine.process( input, outWriter);
     }
-
 
 }
