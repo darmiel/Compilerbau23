788e228602a12bb6ca5387de3da1339f5c752e51
MisterT0815 <larsdebbrecht@gmail.com>
2023-04-06 12:36:21 +0200

    created CharacterliteralMachine

---
+++ TestCharacterliteralMachine.java
@@ -0,0 +1,16 @@
+import java.io.OutputStream;
+
+public class TestCharacterliteralMachine {
+
+    public class TestABMachine {
+
+        public void test( String input ) throws Exception {
+            compiler.StateMachine characterliteralMachine = new compiler.machines.CharacterliteralMachine();
+            java.io.OutputStreamWriter outWriter = new java.io.OutputStreamWriter(System.out);
+            characterliteralMachine.process( input, outWriter);
+        }
+
+    }
+
+
+}
