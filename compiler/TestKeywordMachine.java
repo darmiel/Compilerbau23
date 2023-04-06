5b0fe47f2f3fb2967a7b99b1767d29d95c146cf0
memeToasty <memeToasty@gmail.com>
2023-04-06 20:41:43 +0200

    Fixed typo

--- compiler/TestKeywordMachine.java
+++ compiler/TestKeywordMachine.java
@@ -5,8 +5,8 @@ public class TestKeywordMachine {
     public static final String KEYWORD = "Hallo! Ich bin der Luca! Hackl!";
 
     public static void main(String[] args) throws Exception {
-        compiler.StateMachine abMachine = new compiler.machines.KeywordMachine();
+        compiler.StateMachine keyWordMachine = new compiler.machines.KeywordMachine();
         java.io.OutputStreamWriter outWriter = new java.io.OutputStreamWriter(System.out);
-        abMachine.process(KEYWORD, outWriter);
+        keyWordMachine.process(KEYWORD, outWriter);
     }
 }
