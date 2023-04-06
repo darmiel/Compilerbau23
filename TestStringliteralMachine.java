c69fc26e709eada5ca9fada325b81ce2f4d65835
BenzyBM <vipbence@gmail.com>
2023-04-06 16:17:01 +0200

    Fixed TestStrinliteralMachine

--- TestStringliteralMachine.java
+++ TestStringliteralMachine.java
@@ -4,7 +4,7 @@ public class TestStringliteralMachine {
     System.out.println("HelloWorld");
     compiler.StateMachine StringliteralMachine = new compiler.machines.StringliteralMachine();
     java.io.OutputStreamWriter outWriter = new java.io.OutputStreamWriter(System.out);
-    String Stringliteral = '"'+"Generic Stringliteral"+'"';
+    String Stringliteral = '"'+"!#$%&\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~"+'"';
     char CharArr[] = Stringliteral.toCharArray();
     StringliteralMachine.process(String.copyValueOf(CharArr), outWriter);
   }
