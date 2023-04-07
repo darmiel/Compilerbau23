b5df5a44e6d228ba899f49c772c3adc74c43af69
Moertzschky <karl.moertzschky@sap.com>
2023-04-07 16:40:45 +0200

    fixed newline and enabled basic formulas

--- TestZeilenkommentarMachine.java
+++ TestZeilenkommentarMachine.java
@@ -1,8 +1,9 @@
 public class TestZeilenkommentarMachine {
 
     public static void main(String[] args) throws Exception {
+        System.out.println("Zeilenumbrüche entstehen duch \\n im Test String :)");
         compiler.StateMachine zeilenkkommentarMachine = new compiler.machines.ZeilenkommentarMachine();
         java.io.OutputStreamWriter outWriter = new java.io.OutputStreamWriter(System.out);
-        zeilenkkommentarMachine.process("// Test test test \n", outWriter);
+        zeilenkkommentarMachine.process("// Test + (test) = 0.25 * test \\ \n", outWriter);
     }
 }
--- compiler/machines/ZeilenkommentarMachine.java
+++ compiler/machines/ZeilenkommentarMachine.java
@@ -8,25 +8,39 @@ public class ZeilenkommentarMachine extends compiler.StateMachine{
         addState(startState);
 
         compiler.State oneSlashState = new compiler.State("oneSlash", false);
-        oneSlashState.addTransition('/', "twoSlash");
+        oneSlashState.addTransition('/', "commentState");
         addState(oneSlashState);
 
-        compiler.State twoSlashState = new compiler.State("twoSlash", false);
-        twoSlashState.addTransition('\\', "backSlash");
-        twoSlashState.addTransition(' ', "twoSlash");
-        twoSlashState.addTransitionRange('0','9', "twoSlash");
-        twoSlashState.addTransitionRange('a','z', "twoSlash");
-        twoSlashState.addTransitionRange('A','Z', "twoSlash");
-        addState(twoSlashState);
+        compiler.State commentState = new compiler.State("commentState", false);
+        commentState.addTransition('\n', "end");
+        commentState.addTransition(' ', "commentState");
+        commentState.addTransition('-', "commentState");
+        commentState.addTransition('_', "commentState");
+        commentState.addTransition('*', "commentState");
+        commentState.addTransition('+', "commentState");
+        commentState.addTransition('-', "commentState");
+        commentState.addTransition('=', "commentState");
+        commentState.addTransition('/', "commentState");
+        commentState.addTransition('\\', "commentState");
+        commentState.addTransition('(', "commentState");
+        commentState.addTransition(')', "commentState");
+        commentState.addTransition('.', "commentState");
+        commentState.addTransition(',', "commentState");
+        commentState.addTransitionRange('0','9', "commentState");
+        commentState.addTransitionRange('a','z', "commentState");
+        commentState.addTransitionRange('A','Z', "commentState");
+        addState(commentState);
 
-        compiler.State backSlashState = new compiler.State("backSlash", false);
+
+
+/*        compiler.State backSlashState = new compiler.State("backSlash", false);
         backSlashState.addTransition('n', "end");
         backSlashState.addTransition(' ', "twoSlash");
         backSlashState.addTransitionRange('0','9', "twoSlash");
         backSlashState.addTransitionRange('a','m', "twoSlash");
         backSlashState.addTransitionRange('o','z', "twoSlash");
         backSlashState.addTransitionRange('A','Z', "twoSlash");
-        addState(backSlashState);
+        addState(backSlashState);*/
 
         compiler.State endState = new compiler.State("end", true);
         addState(endState);
