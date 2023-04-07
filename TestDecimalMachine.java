7b661e2900987a566825846652643f1029270ad6
Alex <alex@hagl.de>
2023-04-07 15:46:31 +0200

    Made tests for Decimal Machine

--- TestDecimalMachine.java
+++ TestDecimalMachine.java
@@ -1,42 +1,50 @@
+import compiler.machines.DecimalMachine;
+
 public class TestDecimalMachine {
+
+    private static final String[] acceptCases = {
+        "0.0",
+        "1.5320",
+        "0.5320",
+        "-1.5320",
+        "-0.5320",
+        "-10012323.532",
+        "10012323.532e5",
+        "10012323.532E-5"
+    };
+
+    private static final String[] denyCases = {
+        "0",
+        "01.0",
+        "1.",
+        "01.5320",
+        "01.532",
+        "-01.5320",
+        "-01.532",
+        "10012323.532e-5.5",
+        "10012323.532E-5.5",
+        "10012323.532E05"
+    };
     
     public static void main(String[] args) throws Exception {
-        compiler.StateMachine decimalMachine = new compiler.machines.DecimalMachine();
+        compiler.StateMachine decimalMachine;
         java.io.OutputStreamWriter outWriter = new java.io.OutputStreamWriter(System.out);
-        decimalMachine.process("0.0", outWriter); // accept
-        decimalMachine = new compiler.machines.DecimalMachine();
-        decimalMachine.process("0", outWriter); // no
-        decimalMachine = new compiler.machines.DecimalMachine();
-        decimalMachine.process("01.0", outWriter); // no
-        decimalMachine = new compiler.machines.DecimalMachine();
-        decimalMachine.process("1.", outWriter); // no
-        decimalMachine = new compiler.machines.DecimalMachine();
-        decimalMachine.process("1.5320", outWriter); // accept
-        decimalMachine = new compiler.machines.DecimalMachine();
-        decimalMachine.process("0.5320", outWriter); // accept
-        decimalMachine = new compiler.machines.DecimalMachine();
-        decimalMachine.process("01.5320", outWriter); // no
-        decimalMachine = new compiler.machines.DecimalMachine();
-        decimalMachine.process("01.532", outWriter); // no
-        decimalMachine = new compiler.machines.DecimalMachine();
-        decimalMachine.process("-1.5320", outWriter); // accept
-        decimalMachine = new compiler.machines.DecimalMachine();
-        decimalMachine.process("-0.5320", outWriter); // accept
-        decimalMachine = new compiler.machines.DecimalMachine();
-        decimalMachine.process("-01.5320", outWriter); //no
-        decimalMachine = new compiler.machines.DecimalMachine();
-        decimalMachine.process("-01.532", outWriter); // no
-        decimalMachine = new compiler.machines.DecimalMachine();
-        decimalMachine.process("-10012323.532", outWriter); // acept
-        decimalMachine = new compiler.machines.DecimalMachine();
-        decimalMachine.process("10012323.532e5", outWriter); // acept
-        decimalMachine = new compiler.machines.DecimalMachine();
-        decimalMachine.process("10012323.532e-5.5", outWriter);  //no
-        decimalMachine = new compiler.machines.DecimalMachine();
-        decimalMachine.process("10012323.532E-5.5", outWriter);  //no
-        decimalMachine = new compiler.machines.DecimalMachine();
-        decimalMachine.process("10012323.532E-5", outWriter); //accept
-        decimalMachine = new compiler.machines.DecimalMachine();
-        decimalMachine.process("10012323.532E05", outWriter); // no
+
+        // Accept cases:
+        for(final String input : acceptCases) {
+            decimalMachine = new DecimalMachine();
+            decimalMachine.process(input, outWriter);
+            if(!decimalMachine.isFinalState())
+                throw new Exception("Machine should have returned ACCEPT with input '" + input + "'");
+        }
+
+        // Deny cases:
+        for(final String input : denyCases) {
+            decimalMachine = new DecimalMachine();
+            decimalMachine.process(input, outWriter);
+            if(decimalMachine.isFinalState())
+                throw new Exception("Machine should have returned FAIL with input '" + input + "'");
+
+        }
     }
 }
