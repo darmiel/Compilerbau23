714721023acdafb3f76fd06c93519d165fa9cfc4
Lukas <lukas.raaz@gmail.com>
2023-04-20 12:20:06 +0200

    added Parantheses Parser

--- ParantheseParserMain.java
+++ ParantheseParserMain.java
@@ -1,3 +1,6 @@
+import compiler.Token;
+import compiler.TokenIntf;
+
 import java.io.OutputStreamWriter;
 
 public class ParantheseParserMain {
@@ -6,7 +9,7 @@ public class ParantheseParserMain {
         compiler.Lexer lexer = new compiler.Lexer();
         OutputStreamWriter outStream = new OutputStreamWriter(System.out, "UTF-8");
 
-        lexer.init("(())");
+        lexer.init("((()))");
         parseS(lexer);
         if (lexer.lookAhead().m_type == compiler.TokenIntf.Type.EOF) {
             outStream.write("ACCEPTED");
@@ -18,13 +21,16 @@ public class ParantheseParserMain {
 
     public static void parseS(compiler.Lexer lexer) throws Exception {
         compiler.Token currentToken = lexer.lookAhead();
-        /* if () {
-            // S : (S)
-        } else if () {
+         if (currentToken.m_type == TokenIntf.Type.LPAREN) {
+             lexer.expect(TokenIntf.Type.LPAREN);
+             parseS(lexer);
+             lexer.expect(TokenIntf.Type.RPAREN);
+             // S : (S)
+        } else if (currentToken.m_type == TokenIntf.Type.RPAREN || currentToken.m_type == TokenIntf.Type.EOF) {
             // S : epsilon
         } else {
             lexer.throwCompilerException("invalid paranthese expression", "");
-        } */
+        }
     }
 
 }
