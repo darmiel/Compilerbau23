f178d279c16e770ba6b0839db7aaf8c72f237f50
Alex Hagl <alex@hagl.de>
2023-05-04 10:38:28 +0200

    Added bitshift to parser
    Co-authored-by: Elliot Constantin H <65495509+Proliecan@users.noreply.github.com>
    Co-authored-by: Jas <99250573+jas20202@users.noreply.github.com>
    Co-authored-by: Annalytic-programming <anna.weber.0802@gmail.com>

--- ExpressionParserMain.java
+++ ExpressionParserMain.java
@@ -5,7 +5,7 @@ public class ExpressionParserMain {
     public static void main(String[] args) throws Exception {
         compiler.Lexer lexer = new compiler.Lexer();
         compiler.Parser exprParser = new compiler.Parser(lexer);
-        compiler.ast.ASTExprNode expr = exprParser.parseExpression("423 + 15 - 9 ");
+        compiler.ast.ASTExprNode expr = exprParser.parseExpression("2 << 2 << 2");
         System.out.println(expr.eval());
         OutputStreamWriter outStream = new OutputStreamWriter(System.out, "UTF-8");
         expr.print(outStream, "");
