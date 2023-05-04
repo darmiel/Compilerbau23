bc9c4f56207eb18cc3905169a7134ce8f567769e
darmiel <71837281+darmiel@users.noreply.github.com>
2023-05-04 10:55:03 +0200

    refactor: `AndOr` expression node
    Co-authored-by: TomRomeo <tom.romeo.doil@gmail.com>
    Co-authored-by: Julian <julian70202@gmail.com>

--- compiler/ast/ASTAndOrExprNode.java
+++ compiler/ast/ASTAndOrExprNode.java
@@ -6,29 +6,30 @@ import compiler.TokenIntf;
 import java.io.OutputStreamWriter;
 
 public class ASTAndOrExprNode extends ASTExprNode {
-    private final ASTExprNode m_lhs;
-    private final ASTExprNode m_rhs;
-    private final Token m_token;
+
+    private final ASTExprNode lhs;
+    private final ASTExprNode rhs;
+    private final Token token;
 
     public ASTAndOrExprNode(final ASTExprNode lhs, final ASTExprNode rhs, final Token token) {
-        m_lhs = lhs;
-        m_rhs = rhs;
-        m_token = token;
+        this.lhs = lhs;
+        this.rhs = rhs;
+        this.token = token;
     }
 
     @Override
     public void print(OutputStreamWriter outStream, String indent) throws Exception {
-        outStream.write(indent + m_token.toString() + "\n");
-        String newIndent = indent + "  ";
-        m_lhs.print(outStream, newIndent);
-        m_rhs.print(outStream, newIndent);
+        outStream.write(indent + this.token.toString() + "\n");
+        indent += "  ";
+        this.lhs.print(outStream, indent);
+        this.rhs.print(outStream, indent);
     }
 
     @Override
     public int eval() {
-        final int lhsVal = m_lhs.eval();
-        final int rhsVal = m_rhs.eval();
-        if (m_token.m_type == TokenIntf.Type.AND) {
+        final int lhsVal = this.lhs.eval();
+        final int rhsVal = this.rhs.eval();
+        if (this.token.m_type == TokenIntf.Type.AND) {
             return (lhsVal > 0 && rhsVal > 0) ? 1 : 0;
         }
         return (lhsVal > 0 || rhsVal > 0) ? 1 : 0;
