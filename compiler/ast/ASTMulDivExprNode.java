b5a91a82d4500c28ebe4d1f5e710127e06d78cac
Lukas <lukas.raaz@gmail.com>
2023-05-11 11:50:16 +0200

    added IntrMulDiv and codegen for MulDivExprNode

--- compiler/ast/ASTMulDivExprNode.java
+++ compiler/ast/ASTMulDivExprNode.java
@@ -35,4 +35,12 @@ public class ASTMulDivExprNode extends ASTExprNode{
         }
         return result;
     }
+
+    compiler.InstrIntf codegen(compiler.CompileEnvIntf env) {
+        compiler.InstrIntf lhs = m_lhs.codegen(env);
+        compiler.InstrIntf rhs = m_rhs.codegen(env);
+        compiler.InstrIntf instr = new compiler.instr.InstrMulDiv(m_token.m_type, lhs, rhs);
+        env.addInstr(instr);
+        return instr;
+    }
 }
