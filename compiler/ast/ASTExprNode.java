387ae2b874778697f84a07a2fb11901837c4e241
darmiel <71837281+darmiel@users.noreply.github.com>
2023-05-22 12:08:02 +0200

    feat: added constant folding for plus-minus-expression
    Co-authored-by: Kevin <50447092+4KevR@users.noreply.github.com>
    Co-authored-by: TomRomeo <tom.romeo.doil@gmail.com>
    Co-authored-by: Julian <julian70202@gmail.com>

--- compiler/ast/ASTExprNode.java
+++ compiler/ast/ASTExprNode.java
@@ -1,8 +1,16 @@
 package compiler.ast;
 
+import compiler.info.ConstInfo;
+
 import java.io.OutputStreamWriter;
 
 public abstract class ASTExprNode extends ASTNode {
     public abstract void print(OutputStreamWriter outStream, String indent) throws Exception;
     public abstract int eval();
+
+    // TODO: make this abstract when every subclass implemented this.
+    public ConstInfo constFold() {
+        return new ConstInfo(false, 0);
+    }
+
 }
